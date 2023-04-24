22a55c91281d14ab7bd932b9e5aba024f67f5d71
Jelena Andjelic <jelena.andjelic.gr@gmail.com>
2023-04-24 14:48:58 +0200

    Updated header with sticky navbar

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".header__hamburger");
 const header = document.querySelector(".header");
 const body = document.querySelector("body");
+const navbar = document.querySelector(".header__navbar");
 
 hamburger.addEventListener("click", () => {
   if (header.classList.contains("open")) {
@@ -10,4 +11,13 @@ hamburger.addEventListener("click", () => {
     header.classList.add("open");
     body.classList.add("noscroll");
   }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    navbar.style.opacity = 0.9;
+  } else {
+    navbar.style.opacity = 1;
+  }
+  console.log(window.scrollY);
 });
